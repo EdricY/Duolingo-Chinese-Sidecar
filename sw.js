85ab9fe70e8ac2aaccec37d901a4a59b85833eb2
EdricY <yu.edric@gmail.com>
2026-05-14 17:40:45 -0400

    changes for pwa

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'sidecar-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './words-chinese.json',
+  './words-swahili.json',
+  './icon.svg',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
